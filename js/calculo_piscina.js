83c08d4bf442acdd11158e72f489c27394f48048
crpiscinas <celsoricardo.piscinas@gmail.com>
2024-09-15 11:55:44 -0300

    numeros com pontos

--- js/calculo_piscina.js
+++ js/calculo_piscina.js
@@ -1,3 +1,21 @@
+function formatarNumeroComPontos(numero) {
+  let numeroStr = numero.toString();
+  let resultado = "";
+  let contador = 0;
+
+  // Itera sobre o número de trás para frente
+  for (let i = numeroStr.length - 1; i >= 0; i--) {
+    resultado = numeroStr[i] + resultado;
+    contador++;
+    // Insere o ponto a cada 3 dígitos, mas não no início
+    if (contador % 3 === 0 && i > 0) {
+      resultado = "." + resultado;
+    }
+  }
+
+  return resultado;
+}
+
 let resultado_piscina = document.getElementById("resultado-piscina");
 const formulario_piscina = document.getElementById("formulario_piscina");
 
@@ -28,94 +46,119 @@ function calculoPiscina() {
   let alcalinidade = document.getElementById("alcalinidade");
   let resultadoAlcalinidade = document.getElementById("resultado-alcalinidade");
 
-  resultadoAlcalinidade.innerHTML =
-    15 * +resultadoFinal * +alcalinidade.value.replace(",", ".");
+  if (alcalinidade.value < 100) {
+    resultadoAlcalinidade.innerHTML = formatarNumeroComPontos(
+      Math.abs(
+        (+alcalinidade.value.replace(",", ".") - 100) * 15 * resultadoFinal
+      )
+    );
+  } else {
+    resultadoAlcalinidade.innerHTML = 0;
+  }
 
-  //Alcalinidade
+  //Alcalinidade redutor
   let resultadoAlcalinidade2 = document.getElementById(
     "resultado-alcalinidade2"
   );
 
-  resultadoAlcalinidade2.innerHTML =
-    +alcalinidade.value.replace(",", ".") - 100;
+  if (alcalinidade.value >= 100) {
+    resultadoAlcalinidade2.innerHTML = formatarNumeroComPontos(
+      (+alcalinidade.value.replace(",", ".") - 100) * 15 * resultadoFinal
+    );
+  } else {
+    resultadoAlcalinidade2.innerHTML = 0;
+  }
 
   //Clarificante
   let clarificante = document.getElementById("clarificante");
   let resultadoClarificante = document.getElementById("resultado-clarificante");
 
-  resultadoClarificante.innerHTML =
-    10 * +resultadoFinal * +clarificante.value.replace(",", ".");
+  resultadoClarificante.innerHTML = formatarNumeroComPontos(
+    10 * +resultadoFinal * +clarificante.value.replace(",", ".")
+  );
 
   //ph mais
   let phMais = document.getElementById("ph-mais");
   let resultadoPHMais = document.getElementById("resultado-ph-mais");
 
-  resultadoPHMais.innerHTML =
-    10 * +resultadoFinal * +phMais.value.replace(",", ".");
+  resultadoPHMais.innerHTML = formatarNumeroComPontos(
+    10 * +resultadoFinal * +phMais.value.replace(",", ".")
+  );
 
   //ph menos
   let phMenos = document.getElementById("ph-mais");
   let resultadoPHMenos = document.getElementById("resultado-ph-menos");
 
-  resultadoPHMenos.innerHTML =
-    10 * +resultadoFinal * +phMenos.value.replace(",", ".");
+  resultadoPHMenos.innerHTML = formatarNumeroComPontos(
+    10 * +resultadoFinal * +phMenos.value.replace(",", ".")
+  );
 
   //cloro HTH
   let cloro = document.getElementById("cloro");
   let resultadoCloro = document.getElementById("resultado-cloro");
 
-  resultadoCloro.innerHTML = +resultadoFinal * +cloro.value.replace(",", ".");
+  resultadoCloro.innerHTML = formatarNumeroComPontos(
+    +resultadoFinal * +cloro.value.replace(",", ".")
+  );
 
   //cloro Estabilizado
 
   let resultadoCloroEstabilizado = document.getElementById(
     "resultado-cloro-estabilizado"
   );
-  resultadoCloroEstabilizado.innerHTML =
-    +resultadoFinal * +cloro.value.replace(",", ".");
+  resultadoCloroEstabilizado.innerHTML = formatarNumeroComPontos(
+    +resultadoFinal * +cloro.value.replace(",", ".")
+  );
 
   //Algicida
 
   let algicida = document.getElementById("algicida");
   let resultadoAlgicida = document.getElementById("resultado-algicida");
 
-  resultadoAlgicida.innerHTML =
-    +resultadoFinal * +algicida.value.replace(",", ".");
+  resultadoAlgicida.innerHTML = formatarNumeroComPontos(
+    +resultadoFinal * +algicida.value.replace(",", ".")
+  );
 
   //Sulfato de aluminio genco
   let resultadoAluminio = document.getElementById("resultado-aluminio");
 
-  resultadoAluminio.innerHTML = +resultadoFinal * 20;
+  resultadoAluminio.innerHTML = formatarNumeroComPontos(+resultadoFinal * 20);
 
   //Sulfato de aluminio genco
   let resultadoAluminioOutras = document.getElementById(
     "resultado-aluminio-outras"
   );
 
-  resultadoAluminioOutras.innerHTML = +resultadoFinal * 40;
+  resultadoAluminioOutras.innerHTML = formatarNumeroComPontos(
+    +resultadoFinal * 40
+  );
 
   //Dureza
 
   let dureza = document.getElementById("dureza");
   let resultadoDureza = document.getElementById("resultado-dureza");
 
-  resultadoDureza.innerHTML =
-    +resultadoFinal * +dureza.value.replace(",", ".") * 1.5;
+  resultadoDureza.innerHTML = formatarNumeroComPontos(
+    +dureza.value.replace(",", ".") * 15 * resultadoFinal
+  );
 
   //Acido Cianurico
 
   let acido = document.getElementById("acido");
   let resultadoAcido = document.getElementById("resultado-cianurico");
 
-  resultadoAcido.innerHTML = +resultadoFinal - acido.value.replace(",", ".");
+  resultadoAcido.innerHTML = formatarNumeroComPontos(
+    +resultadoFinal * acido.value.replace(",", ".") * 10
+  );
 
   //Genquest
 
   let genquest = document.getElementById("genquest");
   let resultadoGenquest = document.getElementById("resultado-genquest");
 
-  resultadoGenquest.innerHTML =
-    +resultadoFinal * genquest.value.replace(",", ".");
+  resultadoGenquest.innerHTML = formatarNumeroComPontos(
+    +resultadoFinal * genquest.value.replace(",", ".")
+  );
 }
 setInterval(calculoPiscina, 1000);
 
